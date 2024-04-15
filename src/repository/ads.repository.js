aad6bdcaa0f97fecce0a7a508994b4ea839ddc68
Isham Mohamed <isham.eng@gmail.com>
2024-04-15 12:30:25 +0530

    logging

--- src/repository/ads.repository.js
+++ src/repository/ads.repository.js
@@ -4,6 +4,8 @@ import backendConfig from "../configs/backend.config";
 async function fetchAllAds() {
     try {
         const baseUrl = backendConfig.apiUrl;
+        console.log('baseUrl', baseUrl)
+        console.log('config api url', window.configs.apiUrl)
         const response = await fetch(`${baseUrl}/ads`);
         const data = await response.json();
         return data;
